f5bf320ad3f1eee7f03c9ffe76924d488ede9078
Тарасов Александр <tar-alex@mail.ru>
2025-09-16 16:12:50 +0300

    add install loki and tempo, add common configs

--- provision/mon01.sh
+++ provision/mon01.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update -y
 
 sudo apt-get install -y apt-transport-https software-properties-common wget
@@ -7,8 +9,45 @@ wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt
 
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
-# Updates the list of available packages
 sudo apt-get update
 
-# Installs the latest OSS release:
+echo "install grafana"
+
 sudo apt-get install grafana
+
+echo "install loki"
+
+wget https://github.com/grafana/loki/releases/download/v3.5.5/loki-linux-amd64.zip
+
+unzip loki-linux-amd64.zip
+
+sudo chmod +x loki-linux-amd64
+
+sudo mv loki-linux-amd64 /usr/local/bin/loki
+
+rm loki-linux-amd.zip
+
+echo "create configs for loki"
+
+sudo mkdir -p /etc/loki
+sudo cp /vagrant/configs/loki-config.yaml /etc/loki/config.yaml
+sudo cp /vagrant/systemd/loki.service /etc/systemd/system/
+
+echo "install tempo"
+
+wget https://github.com/grafana/tempo/releases/download/v2.8.2/tempo_2.8.2_linux_amd64.tar.gz
+tar -xvf tempo_2.8.2_linux_amd64.tar.gz
+sudo mv tempo /usr/local/bin/tempo
+rm tempo_2.8.2_linux_amd64.tar.gz
+
+echo "create configs for tempo"
+sudo mkdir -p /etc/loki
+sudo cp /vagrant/configs/tempo-config.yaml /etc/tempo/config.yaml
+sudo cp /vagrant/systemd/tempo.service /etc/systemd/system/
+
+# for update unit-files in systemd
+sudo systemctl daemon-reload
+# for auto start when VMs running
+sudo systemctl enable grafana-server loki tempo
+# for start services
+sudo systemctl restart grafana-server loki tempo
